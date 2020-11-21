7b06ae3acf04f6614dad2f3f246014dca893bee5
Vivekyadv <vivek.yadav2611@gmail.com>
2020-11-21 22:05:56 +0530

    IP validation complete

--- src-rep/blog/views.py
+++ src-rep/blog/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def home(request, *args, **kwargs):
-
 	return render(request,'blog/home.html')
+
+def ip(request, *args, **kwargs):
+	return render (request, 'blog/ip.html')
+
+def mac(request, *args, **kwargs):
+	return render (request, 'blog/mac.html')
--- src-rep/validator/urls.py
+++ src-rep/validator/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import home
+from blog.views import home, ip, mac
 from django.conf import settings
 from django.conf.urls.static import static
+
+from testform.views import iphome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('ip/', ip, name='ip'),
+    path('mac/', mac, name='mac'),
+
+    path('iphome/', iphome, name='iphome'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
